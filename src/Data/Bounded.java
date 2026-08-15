f4826f0edc9d64165c03adead3b22b2f6665b11b
0x1 <kevin.francart3@gmail.com>
2026-08-15 23:02:23 +0200

    misc

---
+++ src/Data/Bounded.java
@@ -0,0 +1,8 @@
+    public static Object topInt = Integer.MAX_VALUE;
+    public static Object bottomInt = Integer.MIN_VALUE;
+
+    public static Object topChar = String.valueOf((char) 65535);
+    public static Object bottomChar = String.valueOf((char) 0);
+
+    public static Object topNumber = Double.POSITIVE_INFINITY;
+    public static Object bottomNumber = Double.NEGATIVE_INFINITY;
--- src/Data/Eq.java
+++ src/Data/Eq.java
@@ -1,7 +1,7 @@
     public static Object eqBooleanImpl = (java.util.function.Function<Object, Object>) (a) -> (java.util.function.Function<Object, Object>) (b) -> ((Boolean) a).equals((Boolean) b);
     public static Object eqIntImpl = (java.util.function.Function<Object, Object>) (a) -> (java.util.function.Function<Object, Object>) (b) -> ((Integer) a).equals((Integer) b);
     public static Object eqStringImpl = (java.util.function.Function<Object, Object>) (a) -> (java.util.function.Function<Object, Object>) (b) -> ((String) a).equals((String) b);
-    public static Object eqCharImpl = (java.util.function.Function<Object, Object>) (a) -> (java.util.function.Function<Object, Object>) (b) -> ((Character) a).equals((Character) b);
+    public static Object eqCharImpl = (java.util.function.Function<Object, Object>) (a) -> (java.util.function.Function<Object, Object>) (b) -> ((String) a).equals((String) b);
     public static Object eqNumberImpl = (java.util.function.Function<Object, Object>) (a) -> (java.util.function.Function<Object, Object>) (b) -> ((Double) a).equals((Double) b);
     public static Object eqArrayImpl = (java.util.function.Function<Object, Object>) (f) -> (java.util.function.Function<Object, Object>) (xs) -> (java.util.function.Function<Object, Object>) (ys) -> {
         Object[] arr1 = (Object[]) xs;
